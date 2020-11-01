a70e6007575dbe7cef5994d751a1002ef5a29cc3
Chandler Van Scoy <chandler@data-head.com>
2020-11-01 13:54:35 -0800

    Export types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
-import {GrayLogger} from "./GrayLogger";
+import { GrayLogger } from "./GrayLogger";
+import { IGraylogConfiguration } from "./Interfaces/IGraylogConfiguration";
+import { IPostData } from "./Interfaces/IPostData";
 
 export default GrayLogger;
+
+export { IGraylogConfiguration, IPostData };
