0c080e0c6443331b34934c63c6d55906ac58f466
Chandler Van Scoy <chandler@data-head.com>
2020-11-01 14:56:21 -0800

    Proper export of GrayLogger class

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,4 @@ import { GrayLogger } from "./GrayLogger";
 import { IGraylogConfiguration } from "./Interfaces/IGraylogConfiguration";
 import { IPostData } from "./Interfaces/IPostData";
 
-export default GrayLogger;
-
-export { IGraylogConfiguration, IPostData };
+export { IGraylogConfiguration, IPostData, GrayLogger };
